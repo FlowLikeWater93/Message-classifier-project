f6e7797246ad31e17a8cf35d6080eac94c5ba0fc
khalidsh93 <mothernature.sa@outlook.com>
2023-10-21 02:37:07 +0300

    modified code in process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -1,6 +1,15 @@
+import sys
 import pandas as pd
 import numpy as np
 import sqlalchemy as db
+
+def load_data(messages_filepath, categories_filepath):
+    print('1- Loading the data')
+    messages_df = pd.read_csv(messages_filepath)
+    categories_df = pd.read_csv(categories_filepath)
+    print(' success ...')
+    return messages_df, categories_df
+    
 
 
 def find_unique_categories(cats):
@@ -18,6 +27,8 @@ def find_unique_categories(cats):
         unique_catgs[i] = unique_catgs[i].split('-')[0]
     return unique_catgs
 
+
+
 def remove_duplicates(df):
     '''
     Parameters : pandas dataframe 
@@ -31,6 +42,8 @@ def remove_duplicates(df):
     if len(ids)>0:
         clean_df = clean_df[clean_df['id'].isin(ids)==False]
     return clean_df.copy()
+
+
 
 def clean_categories (df):
     '''
@@ -61,48 +74,71 @@ def clean_categories (df):
     return pd.DataFrame(data=normalized_categories, columns=unique_catgs)
 
 
-def save_db(df_left, df_right):
+
+def clean_data(messages_df, categories_df):
+    print('\n2- Exploring the data')
+    print(" Let's take a look at the size, columns and datatypes of the messages dataframe\n")
+    print(messages_df.info())
+    print('\n Print the first 10 rows :')
+    print(messages_df.head(10).to_string())
+    print('\n This dataframe has 4 columns : \n 1- message id : a unique numeric identifier\n 2- message : clean message body\n 3-  original : original message mostly in other languages (not english). More than half of the values in this columns are empty        (null values)\n 4- genre : category of message')
+    print("\n Let's display the unique values in column genre")
+    print(messages_df.genre.value_counts())
+    print("\n Now, Let's take a look at categories dataframe")
+    print(categories_df.info())
+    print('\n Print the first 10 rows : ')
+    print(categories_df.head(10).to_string())
+    print('\n We can see that this dataframe has only two columns : \n 1- id : message unique numeric identifier (each id here belongs to a row in the messages dataframe)'
+      '\n 2- categories: categories of each message (1 or more)')
+    print(' Looking at the first 10 rows, we can notice that the all of the values in the categories columns are almost identical. We have multiple Categroies seperated by a ";". each category has either a 0 or 1 next to it, denoting wether the message falls into the category or not (1 = category)')
+    print("\n Let's try to find out how many unique categories we have")
+    print(' We have {} unique message categories'.format(len(find_unique_categories(categories_df.iloc[0,1]))))
+    print('\n3- Cleaning the data : ')
+    print(' 3.1- Dropping duplicates ')
+    messages_df = remove_duplicates(messages_df)
+    categories_df = remove_duplicates(categories_df)
+    print("\n 3.2- Creating 36 columns (one for each unique category)")
+    print(' If message x is in category y only, column y will have value set as 1 and the rest set as 0')
+    clean_categories_df = clean_categories(categories_df)
+    print(clean_categories_df.info())
+    return messages_df.merge(clean_categories_df, on='id', how='inner').copy()
+
+
+def save_data(df, database_filename):
     '''
-    Parameters : two dataframe 
-    - merge the two dataframes on column "id"
+    Parameters : final dataframe 
     - save the final_result into a SQLite database 
     '''
-    final_df = df_left.merge(df_right, on='id', how='inner')
-    print(' saving the final dataframe')
-    engine = db.create_engine('sqlite:///project2.db')
-    final_df.to_sql('project2_messages', engine, index=False)
+    database_name = 'sqlite:///'+database_filename
+    engine = db.create_engine(database_name)
+    df.to_sql('project2_messages', engine, index=False)
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        messages_df, categories_df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(messages_df, categories_df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
     
-    
-# Load the datasets from the two available csv files
-print('1- Loading the data')
-messages_df = pd.read_csv('messages.csv')
-categories_df = pd.read_csv('categories.csv')
-print(' success ...')
-# Explore both dataframes
-print('\n2- Exploring the data')
-print(" Let's take a look at the size, columns and datatypes of the messages dataframe\n")
-print(messages_df.info())
-print('\n Print the first 10 rows :')
-print(messages_df.head(10).to_string())
-print('\n This dataframe has 4 columns : \n 1- message id : a unique numeric identifier\n 2- message : clean message body\n 3- original : original message mostly in other languages (not english). More than half of the values in this columns are empty (null values)\n 4- genre : category of message')
-print("\n Let's display the unique values in column genre")
-print(messages_df.genre.value_counts())
-print("\n Now, Let's take a look at categories dataframe")
-print(categories_df.info())
-print('\n Print the first 10 rows : ')
-print(categories_df.head(10).to_string())
-print('\n We can see that this dataframe has only two columns : \n 1- id : message unique numeric identifier (each id here belongs to a row in the messages dataframe)'
-      '\n 2- categories: categories of each message (1 or more)')
-print(' Looking at the first 10 rows, we can notice that the all of the values in the categories columns are almost identical. We have multiple Categroies seperated by a ";". each category has either a 0 or 1 next to it, denoting wether the message falls into the category or not (1 = category)')
-print("\n Let's try to find out how many unique categories we have")
-print(' We have {} unique message categories'.format(len(find_unique_categories(categories_df.iloc[0,1]))))
-print('\n3- Cleaning the data : ')
-print(' 3.1- Dropping duplicates ')
-messages_df = remove_duplicates(messages_df)
-categories_df = remove_duplicates(categories_df)
-print("\n 3.2- Creating 36 columns (one for each unique category)")
-print(' If message x is in category y only, column y will have value set as 1 and the rest set as 0')
-clean_categories_df = clean_categories(categories_df)
-print(clean_categories_df.info())
-print('\n 3.3- Merging the two dataframes on the id column : ')
-save_db(messages_df, clean_categories_df)
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
